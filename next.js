3396821085842e93d0eec2a3143c961614580ed2
Adeonir Kohl <adeonir@gmail.com>
2024-05-31 11:52:36 -0300

    feat: create linter config for next, node and vite

---
+++ next.js
@@ -0,0 +1,34 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [require.resolve('./_base'), 'next/core-web-vitals', 'next'],
+  env: {
+    node: true,
+    browser: true,
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  rules: {
+    'jsx-a11y/alt-text': ['warn', { elements: ['img'], img: ['Image'] }],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'react/button-has-type': 'warn',
+    'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+    'react/jsx-pascal-case': 'warn',
+    'react/no-unknown-property': 'error',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+  },
+  overrides: [
+    {
+      files: ['src/app/**/*', 'src/**/*.stories.tsx'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
+}
---
+++ vite.js
@@ -0,0 +1,33 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: require.resolve('./_base'),
+  env: {
+    browser: true,
+  },
+  plugins: ['react-refresh', 'react', 'react-hooks', 'jsx-a11y'],
+  rules: {
+    'jsx-a11y/alt-text': ['warn', { elements: ['img'] }],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'react/button-has-type': 'warn',
+    'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+    'react/jsx-pascal-case': 'warn',
+    'react/no-unknown-property': 'error',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+  },
+  overrides: [
+    {
+      files: ['src/app/**/*', 'src/**/*.stories.tsx'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
+}
