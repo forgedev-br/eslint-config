71d753b19499a304d4f133c1c492fc583a5bcfb2
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 10:10:27 -0300

    Update vue rules

--- vue.js
+++ vue.js
@@ -2,13 +2,12 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    require.resolve('./base'),
-    'plugin:vue/vue3-recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-  ],
+  extends: [require.resolve('./base'), 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended'],
   parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint'],
   overrides: [
     {
       files: [
@@ -27,6 +26,18 @@ module.exports = {
       rules: {
         'import/no-default-export': 'off',
         'unicorn/filename-case': ['error', { case: 'pascalCase' }],
+        'vue/html-self-closing': 'off',
+        'vue/max-attributes-per-line': [
+          'error',
+          {
+            singleline: {
+              max: 3,
+            },
+            multiline: {
+              max: 1,
+            },
+          },
+        ],
       },
     },
   ],
